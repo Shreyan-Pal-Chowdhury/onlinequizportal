ec286e8f5d64f26b4811088c26198f9a7073039f
SHREYAN PAL CHOWDHURY <103349527+Shreyan-Pal-Chowdhury@users.noreply.github.com>
2022-10-30 19:51:02 +0530

    feat: add quiz project

---
+++ questions.js
@@ -0,0 +1,128 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which tag is the root element of an HTML document?",
+    answer: "html",
+    options: [
+      "DOCTYPE html",
+      "body",
+      "html",
+      "root"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which type of variable keeps a constant value once it is assigned?",
+    answer: "final",
+    options: [
+      "non-static",
+      "private",
+      "final",
+      "static"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is used to capture data from the physical world in IoT devices?",
+    answer: "sensors",
+    options: [
+      "sensors",
+      "microcontrollers",
+      "actuators",
+      "microprocessors"
+    ]
+  },
+    {
+    numb: 4,
+    question: " What type of computing technology refers to services and applications that typically run on a distributed network through virtualized resources?",
+    answer: "cloud computing",
+    options: [
+      "distributed computing",
+      "cloud computing",
+      "soft computing",
+      "parallel computing"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Choose the correct components of data science.",
+    answer: "all of the above",
+    options: [
+      "domain expertise",
+      "data engineering",
+      "advanced computing",
+      "all of the above"
+    ]
+  },
+  
+  {
+    numb: 6,
+    question: "The runtime system starts your program by calling which function first?",
+    answer: "main",
+    options: [
+      "hello",
+      "iterative",
+      "print",
+      "main"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which statement accurately describes how items are added to and removed from a stack?",
+    answer: "a stacks adds items to the top and removes items from the top.",
+    options: [
+      "a stacks adds items to one side and removes items from the other side.",
+      "a stacks adds items to the top and removes items from the top.",
+      "a stacks adds items to the top and removes items from anywhere in the stack.",
+      "a stacks adds items to either end and removes items from either end."
+    ]
+  },
+  {
+    numb: 8,
+    question: "When using position: fixed, what will the element always be positioned relative to?",
+    answer: "the viewport",
+    options: [
+      "the wrapper element",
+      "the parent element",
+      "the viewport",
+      "the closest element with position: relative"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which style places an element at a fixed location within its container?",
+    answer: "position: absolute",
+    options: [
+      "position: absolute",
+      "display: flex",
+      "float: left",
+      "display: block"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Pick the incorrect statement about inline functions in C++?",
+    answer: "they are generally very large and complicated function",
+    options: [
+      "saves overhead of a return call from a function",
+      "they are generally very large and complicated function",
+      "these functions are inserted/substituted at the point of call",
+      "they reduce function call overheads"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
